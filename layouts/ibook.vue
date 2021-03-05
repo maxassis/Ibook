27cc35a642cf8b8d62a5b1ca2deeda706d4a4c3f
maxassis <max.assis@ymail.com>
2021-03-05 16:32:41 -0300

    iniciando ibook

---
+++ layouts/ibook.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="ibook-layout">
+    <Header />
+    <Container>
+      <Nuxt />
+    </Container>
+    <Footer />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  layout: "ibook"
+});
+</script>
+
+<style lang="scss" scoped></style>
